c132b3c1677cafd831f0266f8a4c427c2b237d9d
Sandro <Me@SandroKodefox.local>
2019-03-07 23:46:54 +0700

    Add size & colors constants

---
+++ src/generals/constants/colors.ts
@@ -0,0 +1,17 @@
+export const GREY = '#808080';
+export const LIGHT_GREY = '#D8D8D8';
+export const LIGHTER_GREY = '#F5F5F5';
+export const BLACK = '#000';
+export const WHITE = '#FFF';
+export const BLACK60 = 'rgba(0, 0, 0, 0.6)';
+export const DARK_GREY50 = 'rgba(45, 45, 45, 0.5)';
+export const PURPLE = '#7D448E';
+export const RED = '#F71552';
+export const RED30 = '#FED5DF';
+
+export const DEFAULT_TEXT_COLOR = BLACK;
+export const LIGHT_TEXT_COLOR = LIGHT_GREY;
+export const DEFAULT_ICON_COLOR = LIGHT_GREY;
+export const DEFAULT_BORDER_COLOR = LIGHT_GREY;
+export const DEFAULT_BACKGROUND_COLOR = WHITE;
+export const DEFAULT_THEME_COLOR = RED;
---
+++ src/generals/constants/size.ts
@@ -0,0 +1,26 @@
+import {Dimensions} from 'react-native';
+
+const {width: WINDOW_WIDTH, height: WINDOW_HEIGHT} = Dimensions.get('window');
+const {width: SCREEN_WIDTH} = Dimensions.get('screen');
+
+const DEFAULT_ICON_SIZE = 24;
+
+const SMALL_FONT_SIZE = 12;
+const DEFAULT_FONT_SIZE = 14;
+const MEDIUM_FONT_SIZE = 16;
+const LARGE_FONT_SIZE = 24;
+const HEADER_FONT_SIZE = 28;
+const BIG_FONT_SIZE = 48;
+
+export {
+  WINDOW_WIDTH,
+  WINDOW_HEIGHT,
+  SCREEN_WIDTH,
+  DEFAULT_ICON_SIZE,
+  DEFAULT_FONT_SIZE,
+  SMALL_FONT_SIZE,
+  MEDIUM_FONT_SIZE,
+  LARGE_FONT_SIZE,
+  HEADER_FONT_SIZE,
+  BIG_FONT_SIZE,
+};

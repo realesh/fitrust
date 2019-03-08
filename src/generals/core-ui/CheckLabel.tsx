8ca0169786a256d60933a4aa5acd83c0dd39d15d
Sandro <Me@sandrokodefox.lan>
2019-03-08 16:21:57 +0700

    [Core UI] CheckLabel & update index

---
+++ src/generals/core-ui/CheckLabel.tsx
@@ -0,0 +1,52 @@
+import React, {ReactNode} from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  ViewProps,
+  GestureResponderEvent,
+} from 'react-native';
+import {Feather as Icon} from '@expo/vector-icons';
+import {SMALL_FONT_SIZE} from '../constants/size';
+import Text from './Text';
+import {LIGHT_GREY, BLUE} from '../constants/colors';
+
+type Props = ViewProps & {
+  /**
+   * controlled value of CheckLabel (REQUIRED)
+   */
+  checked: boolean;
+
+  /**
+   * Function to invoke when button pressed.
+   */
+  onPress: (event: GestureResponderEvent) => void;
+
+  /**
+   * Text children to be displayed.
+   */
+  children?: ReactNode;
+};
+
+const CheckLabel = (props: Props) => {
+  let {checked, onPress, children, style} = props;
+
+  let iconName = checked ? 'check-circle' : 'circle';
+  let color = checked ? BLUE : LIGHT_GREY;
+
+  return (
+    <TouchableOpacity onPress={onPress} style={[styles.container, style]}>
+      <Icon name={iconName} size={14} color={color} style={{marginRight: 5}} />
+      <Text fontSize={SMALL_FONT_SIZE} style={{color}}>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CheckLabel;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+});
--- src/generals/core-ui/index.ts
+++ src/generals/core-ui/index.ts
@@ -1,4 +1,6 @@
+import Button from './Button';
+import CheckLabel from './CheckLabel';
 import Text from './Text';
 import TextInput from './TextInput';
 
-export {Text, TextInput};
+export {Button, CheckLabel, Text, TextInput};

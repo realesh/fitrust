6212e88cf7f248a9e1911e44da1f41100e54f968
Sandro <Me@sandrokodefox.lan>
2019-03-08 16:21:28 +0700

    [Core UI] Text

--- src/generals/core-ui/Text.tsx
+++ src/generals/core-ui/Text.tsx
@@ -1,5 +1,11 @@
 import React, {ReactNode} from 'react';
-import {Text as BaseText, TextProps, StyleSheet} from 'react-native';
+import {
+  Text as BaseText,
+  TextProps,
+  StyleSheet,
+  GestureResponderEvent,
+  TouchableOpacity,
+} from 'react-native';
 import {DEFAULT_FONT_SIZE} from '../constants/size';
 
 type Props = TextProps & {
@@ -7,14 +13,21 @@ type Props = TextProps & {
    * Sets which font family to use. Defaults to regular.
    */
   fontWeight?: 'regular' | 'bold' | 'light';
+
   /*
    * Size of the font. Defaults to 14.
    */
   fontSize?: number;
+
   /*
    * React children.
    */
   children?: ReactNode;
+
+  /*
+   * Function to invoke when text pressed.
+   */
+  onPress?: (event: GestureResponderEvent) => void;
 };
 
 const Text = (props: Props) => {
@@ -23,6 +36,7 @@ const Text = (props: Props) => {
     fontSize = DEFAULT_FONT_SIZE,
     style,
     children,
+    onPress,
     ...otherProps
   } = props;
   let fontFamily =
@@ -31,7 +45,13 @@ const Text = (props: Props) => {
       : fontWeight === 'bold'
       ? styles.bold
       : styles.light;
-  return (
+  return onPress ? (
+    <TouchableOpacity onPress={onPress}>
+      <BaseText style={[fontFamily, {fontSize}, style]} {...otherProps}>
+        {children}
+      </BaseText>
+    </TouchableOpacity>
+  ) : (
     <BaseText style={[fontFamily, {fontSize}, style]} {...otherProps}>
       {children}
     </BaseText>

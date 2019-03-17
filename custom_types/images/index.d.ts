9c69b3d96b976a4f0be7963e05b95b45b0a55d93
SandroKodefox <Me@SandroKodefox.local>
2019-03-17 13:34:38 +0700

    [Core UI] Edit Avatar default source

--- custom_types/images/index.d.ts
+++ custom_types/images/index.d.ts
@@ -6,6 +6,10 @@ declare module '*.jpg' {
   const value: any;
   export default value;
 }
+declare module '*.jpeg' {
+  const value: any;
+  export default value;
+}
 declare module '*.svg' {
   const value: any;
   export default value;
--- src/generals/core-ui/Avatar.tsx
+++ src/generals/core-ui/Avatar.tsx
@@ -8,7 +8,7 @@ import {
   GestureResponderEvent,
   View,
 } from 'react-native';
-import DEFAULT_AVATAR from '../../assets/images/default-avatar.png';
+import MALE_AVATAR from '../../assets/images/male-avatar.jpeg';
 import {AVATAR_SIZE_BIG, AVATAR_SIZE_SMALL} from '../constants/size';
 
 type Props = {
@@ -57,17 +57,22 @@ type Props = {
 export default function Avatar(props: Props) {
   let {source, size, style, onPress} = props;
   return onPress ? (
-    <TouchableOpacity style={style} onPress={onPress}>
+    <TouchableOpacity
+      style={[styles[size], styles.shadow, style]}
+      onPress={onPress}
+    >
       <Image
-        source={source ? {uri: source} : DEFAULT_AVATAR}
+        source={source ? {uri: source} : MALE_AVATAR}
         style={styles[size]}
+        resizeMethod="scale"
       />
     </TouchableOpacity>
   ) : (
-    <View style={style}>
+    <View style={[styles[size], styles.shadow, style]}>
       <Image
-        source={source ? {uri: source} : DEFAULT_AVATAR}
+        source={source ? {uri: source} : MALE_AVATAR}
         style={styles[size]}
+        resizeMethod="scale"
       />
     </View>
   );
@@ -84,8 +89,12 @@ const styles = StyleSheet.create({
     width: AVATAR_SIZE_BIG,
     borderRadius: AVATAR_SIZE_BIG / 2,
   },
-  image: {
-    flex: 1,
-    width: '100%',
+  shadow: {
+    shadowColor: '#ccc',
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    shadowOffset: {width: 0, height: 2},
+    elevation: 5,
+    backgroundColor: 'grey',
   },
 });

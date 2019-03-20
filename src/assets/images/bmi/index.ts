bc6b499d557e3166206dccb261938873603e55b0
SandroKodefox <Me@SandroKodefox.local>
2019-03-20 13:12:49 +0700

    Add Gender & THumb images

---
+++ src/assets/images/bmi/index.ts
@@ -0,0 +1,7 @@
+import maleActive from './male-active.png';
+import maleInactive from './male-inactive.png';
+import femaleActive from './female-active.png';
+import femaleInactive from './female-inactive.png';
+import sliderThumb from './slider-thumb.png';
+
+export {maleActive, maleInactive, femaleActive, femaleInactive, sliderThumb};

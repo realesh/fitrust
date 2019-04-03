5c93c4105f426e4d2e78233b91304643b9b6d216
SandroKodefox <realme.esh@gmail.com>
2019-04-03 22:36:53 +0700

    [Dependency] react-native-countdown-component & typing modules

---
+++ custom_types/react-native-countdown-component/index.d.ts
@@ -0,0 +1,30 @@
+declare module 'react-native-countdown-component' {
+  import {StyleProp, ViewStyle} from 'react-native';
+  import {Component} from 'react';
+
+  type TimeType = 'D' | 'H' | 'M' | 'S';
+  type TimeLabels = {
+    d?: string;
+    h?: string;
+    m?: string;
+    s?: string;
+  };
+
+  type CountdownProps = {
+    style?: StyleProp<ViewStyle>;
+    digitStyle?: StyleProp<ViewStyle>;
+    digitTxtStyle?: StyleProp<TextStyle>;
+    timeLabelStyle?: StyleProp<TextStyle>;
+    separatorStyle?: StyleProp<ViewStyle>;
+    size?: number;
+    until: number;
+    onFinish: () => void | null;
+    onChange?: () => void | null;
+    timeToShow: Array<TimeType>;
+    timeLabels?: TimeLabels;
+    showSeparator?: boolean;
+    running?: boolean;
+  };
+
+  export default class CountDown extends Component<CountdownProps> {}
+}

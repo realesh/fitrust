2b4e75f456666b1d77f3d78365c2f4c47621cec4
SandroKodefox <realme.esh@gmail.com>
2019-04-10 16:44:52 +0700

    Update countdown Index

--- src/generals/components/index.ts
+++ src/generals/components/index.ts
@@ -1,5 +1,15 @@
 import Swiper from './Swiper';
 import Toolbar from './Toolbar';
 import ProgressWithLabel from './ProgressBarWithLabel';
+import PopupInfoDialog from './PopupInfoDialog';
+import PopupDialog from './PopupDialog';
+import AnimatedChevron from './AnimatedChevron';
 
-export {Swiper, Toolbar, ProgressWithLabel};
+export {
+  Swiper,
+  Toolbar,
+  ProgressWithLabel,
+  PopupInfoDialog,
+  PopupDialog,
+  AnimatedChevron,
+};

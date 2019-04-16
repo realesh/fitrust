8c2a8b2a5349f88260a8119aea2dd8abb0af410d
SandroKodefox <realme.esh@gmail.com>
2019-04-16 17:06:54 +0700

    [Assets] add BMR related images

--- src/assets/images/bmr/index.ts
+++ src/assets/images/bmr/index.ts
@@ -3,5 +3,13 @@ import maleInactive from './male-inactive.png';
 import femaleActive from './female-active.png';
 import femaleInactive from './female-inactive.png';
 import sliderThumb from './slider-thumb.png';
+import bmrCalcResult from './bmr-result.png';
 
-export {maleActive, maleInactive, femaleActive, femaleInactive, sliderThumb};
+export {
+  maleActive,
+  maleInactive,
+  femaleActive,
+  femaleInactive,
+  sliderThumb,
+  bmrCalcResult,
+};

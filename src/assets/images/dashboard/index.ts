c6e61a3d2d412823061d318ef37b79527be542db
SandroKodefox <realme.esh@gmail.com>
2019-04-20 19:43:27 +0700

    [Assets] Add walk icon

--- src/assets/images/dashboard/index.ts
+++ src/assets/images/dashboard/index.ts
@@ -1,5 +1,6 @@
 import food from './food.png';
 import fire from './fire.png';
 import fireGrey from './fire-grey.png';
+import walk from './walk.png';
 
-export {food, fire, fireGrey};
+export {food, fire, fireGrey, walk};

b2665f9057fbef89f04bebd5b966d70624f5d569
SandroKodefox <realme.esh@gmail.com>
2019-04-21 14:47:29 +0700

    [Assets] Add water images

--- src/assets/images/dashboard/index.ts
+++ src/assets/images/dashboard/index.ts
@@ -2,5 +2,7 @@ import food from './food.png';
 import fire from './fire.png';
 import fireGrey from './fire-grey.png';
 import walk from './walk.png';
+import waterActive from './water-active.png';
+import waterInactive from './water-inactive.png';
 
-export {food, fire, fireGrey, walk};
+export {food, fire, fireGrey, walk, waterActive, waterInactive};

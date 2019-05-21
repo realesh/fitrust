b4dbbe6b76e7aa322452cf0590fd4b682b852806
SandroKodefox <realme.esh@gmail.com>
2019-05-21 12:49:36 +0700

    [Assets] Add avatar images

---
+++ src/assets/images/profile/avatars/index.ts
@@ -0,0 +1,23 @@
+import avatar1 from './avatar1.jpeg';
+import avatar2 from './avatar2.jpeg';
+import avatar3 from './avatar3.jpeg';
+import avatar4 from './avatar4.jpeg';
+import avatar5 from './avatar5.jpeg';
+import avatar6 from './avatar6.jpeg';
+import avatar7 from './avatar7.jpeg';
+import avatar8 from './avatar8.jpeg';
+import avatar9 from './avatar9.jpeg';
+
+let avatarStocks = [
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+  avatar8,
+  avatar9,
+];
+
+export {avatarStocks};

29abbe2b6d03ee5a175ad4256f284fe7f7a4ad44
SandroKodefox <realme.esh@gmail.com>
2019-05-22 00:35:05 +0700

    [Fetcher] ExerciseMode

---
+++ src/helpers/Fetchers/fetchExerciseMode.ts
@@ -0,0 +1,35 @@
+type DataItem = {
+  time: string;
+  value: number;
+};
+
+type HeartActivitesIntraday = {
+  dataset?: Array<DataItem>;
+};
+
+export type HeartActivitiesResponse = {
+  'activities-heart-intraday'?: HeartActivitesIntraday;
+};
+
+let fetchExerciseMode = async (
+  userID: string,
+  accessToken: string,
+  startTime: string,
+  finishTime: string,
+): Promise<HeartActivitiesResponse> => {
+  let response = await fetch(
+    `https://api.fitbit.com/1/user/${userID}/activities/heart/date/today/1d/1min/time/${startTime}/${finishTime}.json`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+
+  let responseJson: HeartActivitiesResponse = await response.json();
+  return responseJson;
+};
+
+export default fetchExerciseMode;

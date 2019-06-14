1bda9f3d301c86bb0be82e64639adaf9dd091e0e
SandroKodefox <realme.esh@gmail.com>
2019-06-14 16:59:23 +0700

    Update assets onboarding

--- src/assets/images/onBoarding/index.ts
+++ src/assets/images/onBoarding/index.ts
@@ -1,5 +1,15 @@
 import fitnessTracker from './fitness-tracker.png';
 import heartRateDoctor from './heart-rate.png';
 import personalTrainer from './personal-trainer.png';
+import largeBurger from './large-burger.png';
+import hero from './hero-sword.png';
+import celebration from './celebration.png';
 
-export {fitnessTracker, heartRateDoctor, personalTrainer};
+export {
+  fitnessTracker,
+  heartRateDoctor,
+  personalTrainer,
+  largeBurger,
+  hero,
+  celebration,
+};

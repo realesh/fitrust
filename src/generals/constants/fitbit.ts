4170fe7614b399b0fee6234fea6b7f4c52ff6711
Samuel Sandro <34760718+realesh@users.noreply.github.com>
2019-06-15 18:28:20 +0700

    Delete fitbit.ts

--- src/generals/constants/fitbit.ts
+++
@@ -1,6 +0,0 @@
-const FITBIT_APP_ID = '22DH6B';
-const FITBIT_AUTH_URL =
-  'https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DH6B&redirect_uri=http%3A%2F%2F127.0.0.1%3A8080%2F&scope=activity%20heartrate%20nutrition%20profile%20weight&expires_in=604800';
-const EXPIRED_DURATION = 2592000;
-
-export {FITBIT_APP_ID, FITBIT_AUTH_URL, EXPIRED_DURATION};
